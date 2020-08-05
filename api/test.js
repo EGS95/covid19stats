34343a840935670ec4204328536ae11c7ec56f0d
egs95 <elie.g.saab@gmail.com>
2020-08-05 08:54:59 +0300

    added api

--- api/test.js
+++ api/test.js
@@ -1,6 +1,10 @@
+const fetch = require('node-fetch')
+
 module.exports = (req,res) => {
-    const key = process.env.IP_INFO
-    res.json({
-        key:key
-    })
+    fetch(`https://ipinfo.io/?token=${process.env.IP_INFO}`)
+    .then((res) => res.json())
+    .then((data) => {
+     
+      res.json(data)
+    });
 }
