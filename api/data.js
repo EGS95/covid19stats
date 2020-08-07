b1c77d14033ba47b5d545a53e7b1b9604b224a63
egs95 <elie.g.saab@gmail.com>
2020-08-08 01:12:29 +0300

    update api

--- api/data.js
+++ api/data.js
@@ -7,28 +7,27 @@ module.exports = async (req, res) => {
   let reqCountry = "";
   try {
     let countryData = await ipinfo.lookupIp(ip);
-    reqCountry = countryData._countryCode
-    console.log(reqCountry)
+    reqCountry = countryData._countryCode;
   } catch {
     reqCountry = "";
   }
 
+  try {
+    let data = await Promise.all([
+      fetch("https://disease.sh/v3/covid-19/all?yesterday=false").then((res) =>
+        res.json()
+      ),
+      fetch(
+        "https://disease.sh/v3/covid-19/countries?yesterday=false"
+      ).then((res) => res.json()),
+    ]);
 
-  try{
-      let data = await Promise.all([
-        fetch("https://disease.sh/v3/covid-19/all?yesterday=false").then(res => res.json()),
-        fetch("https://disease.sh/v3/covid-19/countries?yesterday=false").then(res => res.json()),
-      ]);
-
-      res.json({
-        countryCode: reqCountry,
-        data,
-      });
+    res.json({
+      countryCode: reqCountry,
+      worldData:data[0],
+      countryData:data[1]
+    });
+  } catch (err) {
+    res.json({ Error: err.message });
   }
-
-  catch(err) {
-      res.json({Error:err.message})
-  }
-
-
 };
