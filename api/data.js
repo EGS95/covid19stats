90e2c7869d5d51c3e923fe53c4bd3ac96c876b84
egs95 <elie.g.saab@gmail.com>
2020-08-10 07:51:44 +0300

    update api

--- api/data.js
+++ api/data.js
@@ -3,8 +3,31 @@ const Ipinfo = require("node-ipinfo");
 const ipinfo = new Ipinfo(process.env.IP_INFO);
 
 module.exports = async (req, res) => {
+  if (req.query.ccode) {
+    fetch(
+      `https://disease.sh/v3/covid-19/historical/${req.query.ccode}?lastdays=all`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        let keys = Object.keys(data.timeline.cases);
+        let cases = Object.values(data.timeline.cases);
+        let deaths = Object.values(data.timeline.deaths);
+        let recovered = Object.values(data.timeline.recovered);
 
-  res.send(req.query)
+        let historicalData = keys.map((key, index) => {
+          return {
+            date: key,
+            cases: cases[index],
+            deaths: deaths[index],
+            recovered: recovered[index],
+          };
+        });
+
+        res.json({
+          historicalData,
+        });
+      });
+  }
 
   const ip = req.headers["x-forwarded-for"];
   let reqCountry = "";
@@ -28,26 +51,25 @@ module.exports = async (req, res) => {
       ).then((res) => res.json()),
     ]);
 
-    let keys = Object.keys(data[2].timeline.cases)
-        let cases = Object.values(data[2].timeline.cases)
-        let deaths = Object.values(data[2].timeline.deaths)
-        let recovered = Object.values(data[2].timeline.recovered)
+    let keys = Object.keys(data[2].timeline.cases);
+    let cases = Object.values(data[2].timeline.cases);
+    let deaths = Object.values(data[2].timeline.deaths);
+    let recovered = Object.values(data[2].timeline.recovered);
 
-
-        let historicalData =keys.map((key,index) => {
-          return {
-            date:key,
-            cases:cases[index],
-            deaths:deaths[index],
-            recovered:recovered[index]
-          }
-        })
+    let historicalData = keys.map((key, index) => {
+      return {
+        date: key,
+        cases: cases[index],
+        deaths: deaths[index],
+        recovered: recovered[index],
+      };
+    });
 
     res.json({
       countryCode: reqCountry,
       worldData: data[0],
       countryData: data[1],
-      historicalData
+      historicalData,
     });
   } catch (err) {
     res.json({ Error: err.message });
