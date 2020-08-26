b6125efd5fdd64cb75ff18a5c017f301d38a3629
egs95 <elie.g.saab@gmail.com>
2020-08-26 22:36:10 +0300

    update countrystats

--- src/components/CountryStats/CountryStats.js
+++ src/components/CountryStats/CountryStats.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Box, Typography, Select, MenuItem, Paper } from "@material-ui/core";
 import { FlagOutlined } from "@material-ui/icons";
-import { lightBlue, green, pink } from "@material-ui/core/colors";
 import { withStyles } from "@material-ui/core";
 import {
   PieChart,
@@ -20,7 +19,7 @@ import {
 import ReactCountryFlag from "react-country-flag";
 import myStyle from "./Style";
 
-const colors = [lightBlue[600], pink["A400"], green["A700"]];
+const colors = ["#039be5", "#f50057", "#00c853"];
 const radian = Math.PI / 180;
 
 const renderCustomizedLabel = (props) => {
@@ -33,7 +32,7 @@ const renderCustomizedLabel = (props) => {
     payload,
     percent,
   } = props;
-  let pos = 0.5;
+  let pos;
   switch (payload.name) {
     case "Deaths":
       pos = 0.8;
@@ -115,163 +114,153 @@ class CountryStats extends Component {
       return null;
     };
 
-    if (country) {
-      const data = [
-        {
-          name: "Active",
-          value: country.cases - (country.deaths + country.recovered),
-        },
-        { name: "Deaths", value: country.deaths },
-        { name: "Recovered", value: country.recovered },
-      ];
+    const data = [
+      {
+        name: "Active",
+        value: country.cases - (country.deaths + country.recovered),
+      },
+      { name: "Deaths", value: country.deaths },
+      { name: "Recovered", value: country.recovered },
+    ];
 
-      const legendData = data.map((item, index) => ({
-        id: item.name,
-        type: "circle",
-        value: (
-          <span>
-            {item.name}
-            <p
-              style={{
-                color: colors[index],
-                fontSize: "15px",
-                fontWeight: "bold",
-              }}
+    const legendData = data.map((item, index) => ({
+      id: item.name,
+      type: "circle",
+      value: (
+        <span>
+          {item.name}
+          <p
+            style={{
+              color: colors[index],
+              fontSize: "15px",
+              fontWeight: "bold",
+            }}
+          >
+            ( {item.value} )
+          </p>
+        </span>
+      ),
+      color: colors[index],
+    }));
+
+    return (
+      <Box component={Paper} className={classes.container}>
+        <Box display="flex" alignItems="center">
+          <FlagOutlined className={classes.worldIcon} />
+          <Typography variant="h4">Country stats</Typography>
+        </Box>
+        <Box className={classes.statsWrapper}>
+          <Box className={classes.selectWrapper}>
+            <Typography variant="h5" paragraph>
+              Choose a country:
+            </Typography>
+            <Select
+              variant="outlined"
+              value={country.country}
+              onChange={this.handleChange}
+              className={classes.select}
             >
-              ( {item.value} )
-            </p>
-          </span>
-        ),
-        color: colors[index],
-      }));
-
-      return (
-        <Box component={Paper} className={classes.container}>
-          <Box display="flex" alignItems="center">
-            <FlagOutlined className={classes.worldIcon} />
-            <Typography variant="h4">Country stats</Typography>
+              {countries.map((country, index) => {
+                return (
+                  <MenuItem key={index} value={country.country}>
+                    {country.countryInfo.iso2 && (
+                      <ReactCountryFlag
+                        svg
+                        countryCode={country.countryInfo.iso2}
+                        className={classes.flag}
+                      />
+                    )}
+                    {country.country}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+            <Typography
+              paragraph
+              align="center"
+              variant="h4"
+              color="textPrimary"
+            >{`Total cases: ${country.cases}`}</Typography>
+            <Typography
+              paragraph
+              align="center"
+              variant="h6"
+              color="primary"
+            >{`Today cases: ${country.todayCases}`}</Typography>
+            <Typography
+              paragraph
+              align="center"
+              variant="h6"
+              color="secondary"
+            >{`Today deaths: ${country.todayDeaths}`}</Typography>
+            <Typography
+              paragraph
+              align="center"
+              variant="h6"
+              style={{ color: colors[2] }}
+            >{`Today recovered: ${country.todayRecovered}`}</Typography>
           </Box>
-          <Box className={classes.statsWrapper}>
-            <Box className={classes.selectWrapper}>
-              <Typography variant="h5" paragraph>
-                Choose a country:
-              </Typography>
-              <Select
-                variant="outlined"
-                value={country.country}
-                onChange={this.handleChange}
-                className={classes.select}
-              >
-                {countries.map((country, index) => {
-                  return (
-                    <MenuItem key={index} value={country.country}>
-                      {country.countryInfo.iso2 && (
-                        <ReactCountryFlag
-                          svg
-                          countryCode={country.countryInfo.iso2}
-                          className={classes.flag}
-                        />
-                      )}
-                      {country.country}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-              <Typography
-                paragraph
-                align="center"
-                variant="h4"
-                color="textPrimary"
-              >{`Total cases: ${country.cases}`}</Typography>
-              <Typography
-                paragraph
-                align="center"
-                variant="h6"
-                color="primary"
-              >{`Today cases: ${country.todayCases}`}</Typography>
-              <Typography
-                paragraph
-                align="center"
-                variant="h6"
-                color="secondary"
-              >{`Today deaths: ${country.todayDeaths}`}</Typography>
-              <Typography
-                paragraph
-                align="center"
-                variant="h6"
-                style={{ color: colors[2] }}
-              >{`Today recovered: ${country.todayRecovered}`}</Typography>
-            </Box>
 
-            <Box
-              width={{ xs: "100%", sm: "50%" }}
-              height={{ xs: 300, sm: 400 }}
-            >
-              <ResponsiveContainer>
-                <PieChart>
-                  <Pie
-                    minAngle={3}
-                    innerRadius={5}
-                    outerRadius="95%"
-                    data={data}
-                    labelLine={false}
-                    label={renderCustomizedLabel}
-                    dataKey="value"
-                  >
-                    {data.map((entry, index) => (
-                      <Cell key={`cell-${index}`} fill={colors[index]} />
-                    ))}
-                  </Pie>
-                  <Legend verticalAlign="top" payload={legendData} />
-                </PieChart>
-              </ResponsiveContainer>
-            </Box>
-          </Box>
-          <Box width={"100%"} height={400}>
+          <Box width={{ xs: "100%", sm: "50%" }} height={{ xs: 300, sm: 400 }}>
             <ResponsiveContainer>
-              <LineChart
-                width={500}
-                height={300}
-                data={plotData}
-                margin={{
-                  top: 20,
-                  right: 30,
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="date" />
-                <YAxis />
-                <Tooltip content={<CustomTooltip />} />
-                <Legend />
-                <Line
-                  type="monotone"
-                  dataKey="cases"
-                  stroke="#039be5"
-                  dot={false}
-                  strokeWidth={3}
-                />
-                <Line
-                  type="monotone"
-                  dataKey="deaths"
-                  stroke="#f50057"
-                  dot={false}
-                  strokeWidth={3}
-                />
-                <Line
-                  type="monotone"
-                  dataKey="recovered"
-                  stroke="#00c853"
-                  dot={false}
-                  strokeWidth={3}
-                />
-
-                <Brush height={25} travellerWidth={20} />
-              </LineChart>
+              <PieChart>
+                <Pie
+                  minAngle={3}
+                  innerRadius={5}
+                  outerRadius="95%"
+                  data={data}
+                  labelLine={false}
+                  label={renderCustomizedLabel}
+                  dataKey="value"
+                >
+                  {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={colors[index]} />
+                  ))}
+                </Pie>
+                <Legend verticalAlign="top" payload={legendData} />
+              </PieChart>
             </ResponsiveContainer>
           </Box>
         </Box>
-      );
-    } else return null;
+        <Box width={"100%"} height={400}>
+          <ResponsiveContainer>
+            <LineChart
+              data={plotData}
+              margin={{
+                top: 20,
+                right: 30,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="date" />
+              <YAxis />
+              <Tooltip content={<CustomTooltip />} />
+              <Legend />
+              <Line
+                dataKey="cases"
+                stroke="#039be5"
+                dot={false}
+                strokeWidth={3}
+              />
+              <Line
+                dataKey="deaths"
+                stroke="#f50057"
+                dot={false}
+                strokeWidth={3}
+              />
+              <Line
+                dataKey="recovered"
+                stroke="#00c853"
+                dot={false}
+                strokeWidth={3}
+              />
+
+              <Brush height={25} travellerWidth={20} />
+            </LineChart>
+          </ResponsiveContainer>
+        </Box>
+      </Box>
+    );
   }
 }
 
